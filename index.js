eee85762179033855671089f58cd6342c55b1beb
munibakami <163196061+munibakami@users.noreply.github.com>
2024-07-15 09:20:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+console.log("WELLcome to cli number gussing game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "Enter your guess number(Number limit from 1 to 10)",
+    },
+]);
+if (answer.userGussedNumber === randomNumber) {
+    console.log("Congratulation ! you guess a correct number.");
+}
+else {
+    console.log("sorry, you guess a wrong number");
+}
